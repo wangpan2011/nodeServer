cb2868da59f9d3f0d58cd188c3defc5b0e778ff8
wangpan <wpmale@gmail.com>
2018-05-07 18:25:52 +0800

    add barbel配置

--- test.js
+++ test.js
@@ -1,19 +1,2 @@
-// const USER = require('./database/user/userDbHandler');
-// var test = async () => {
-//         var created = await USER.getOrCreateUser('lipan', '123456');
-//         console.log(`结果是：${created}`)
-//         if(created) {
-//             console.log("创建用户成功：" + created);
-//         } else {
-//             console.log("创建用户失败1");
-//         }
-// };
-// test().then((result)=>{
-//     console.log(`success: ${result}`);
-// }, (err)=>{
-//     console.log(`err: ${err}`);
-// });
-const logger = require('./utils/logger');
-logger.debug("debug");
-logger.error("error");
-logger.info("info");
+import {sum} from './module1';
+console.log(sum(1, 2));
